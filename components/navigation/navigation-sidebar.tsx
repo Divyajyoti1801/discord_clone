d6723a3cff779e07cdf712fce2b2951c13fc1030
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-03 22:21:01 +0530

    CREATE_SERVER_MODAL : completed

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -31,12 +31,14 @@ export const NavigationSidebar = async () => {
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
       <ScrollArea className="flex-1 w-full">
         {servers.map((server) => (
-          <NavigationItem
-            key={server.id}
-            id={server.id}
-            name={server.name}
-            imageUrl={server.imageUrl}
-          />
+          <div className="mb-4" key={server.id}>
+            <NavigationItem
+              key={server.id}
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
         ))}
       </ScrollArea>
       <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
